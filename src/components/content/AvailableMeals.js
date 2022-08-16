72eba6942bd880ebd283265166d49b4c833736a8
Mithin <mithingthomas@gmail.com>
2022-08-16 14:23:56 -0400

    handle loading state

--- src/components/content/AvailableMeals.js
+++ src/components/content/AvailableMeals.js
@@ -8,6 +8,7 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -26,9 +27,14 @@ const AvailableMeals = () => {
         );
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return <section className={classes.mealsLoading}><p>Loading...</p></section>;
+  }
 
   const mealsList = meals.map(meal =>
     <MealItem
